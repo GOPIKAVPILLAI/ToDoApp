ba1085d4fe01d7bad7abf97044838a835cc96189
Gopika V Pillai <160047839+GOPIKAVPILLAI@users.noreply.github.com>
2025-01-22 18:14:48 +0530

    User create and JWT token (#4)

--- Todos/models.py
+++ Todos/models.py
@@ -1,7 +1,18 @@
 from Todos.database import Base
-from sqlalchemy import Column,Integer,Boolean,String
-
-
+from sqlalchemy import Column,Integer,Boolean,String,ForeignKey
+class Users(Base):
+    
+    __tablename__='users'
+    id=Column(Integer,primary_key=True,index=True)
+    email=Column(String,unique=True)
+    first_name=Column(String,max_length=100)
+    last_name=Column(String,max_length=100)
+    hashed_password=Column(String)
+    is_active=Column(Boolean,default=True)
+    role=Column(String)
+    
+    
+    
 class ToDos(Base):
 
     __tablename__ = 'todos'
@@ -10,3 +21,4 @@ class ToDos(Base):
     description=Column(String)
     priority=Column(Integer)
     complete=Column(Boolean,default=False)
+    owner=Column(Integer,ForeignKey('users.id'))
